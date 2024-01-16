3b14b364717d0d215a915d48a1ab0e68a896f5c6
Nikolay Vlasov <vlason@amazon.com>
2024-01-16 14:27:28 +1100

    Refactoring based on the feedback on dubious arguments for solana-validator #pr-28

--- lib/solana/lib/assets/solana/node-heavy-rpc-template.sh
+++ lib/solana/lib/assets/solana/node-heavy-rpc-template.sh
@@ -4,9 +4,13 @@ set -o nounset
 set -o pipefail
 # Remove empty snapshots
 find "/var/solana/data/ledger" -name "snapshot-*" -size 0 -print -exec rm {} \; || true
-export RUST_LOG=warning
+export RUST_LOG=error
 export RUST_BACKTRACE=full
 export SOLANA_METRICS_CONFIG=__SOLANA_METRICS_CONFIG__
+
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+export EC2_INTERNAL_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
 /home/solana/bin/solana-validator \
 --ledger /var/solana/data/ledger \
 --identity /home/solana/config/validator-keypair.json \
@@ -16,8 +20,10 @@ __ENTRY_POINTS__ \
 --no-voting \
 --full-rpc-api \
 --rpc-port 8899 \
---gossip-port 8801 \
---dynamic-port-range 8800-8813 \
+--gossip-port 8800 \
+--dynamic-port-range 8800-8814 \
+--private-rpc \
+--rpc-bind-address $EC2_INTERNAL_IP \
 --wal-recovery-mode skip_any_corrupted_record \
 --enable-rpc-transaction-history \
 --enable-cpi-and-log-storage \
