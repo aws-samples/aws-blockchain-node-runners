2e60720017148dc83ad36a631f7680b8608b7011
Ahmed Ghonima <aghonima@amazon.de>
2024-05-23 09:39:00 +0200

    Read snapshot url correctly

--- lib/tezos/lib/assets/download-snapshot.sh
+++ lib/tezos/lib/assets/download-snapshot.sh
@@ -3,13 +3,13 @@ set +e
 
 source /etc/environment
 
-echo "Downloading TZ snapshot from marigold."
+echo "Downloading TZ snapshot from $TZ_SNAPSHOTS_URI."
 
 
 TZ_SNAPSHOTS_FILE_NAME=snapshot
 TZ_SNAPSHOTS_DIR=~/.tezos-node/node
 
-wget -O /tmp/$TZ_SNAPSHOTS_FILE_NAME https://snapshots.tezos.marigold.dev/api/mainnet/rolling
+wget -O /tmp/$TZ_SNAPSHOTS_FILE_NAME $TZ_SNAPSHOTS_URI
 octez-node snapshot import /tmp/$TZ_SNAPSHOTS_FILE_NAME --data-dir $TZ_SNAPSHOTS_DIR --no-check
 rm /tmp/$TZ_SNAPSHOTS_FILE_NAME
 
