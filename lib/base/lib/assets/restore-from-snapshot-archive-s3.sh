ec2d273e99fd5b4c3866829fadd5c7ef3b651e7b
Nikolay Vlasov <vlason@amazon.com>
2024-05-29 11:33:28 +1000

    Base. Refactoring restoration of Archive nodes from snapshots

--- lib/base/lib/assets/restore-from-snapshot-archive-s3.sh
+++ lib/base/lib/assets/restore-from-snapshot-archive-s3.sh
@@ -11,7 +11,7 @@ SNAPSHOT_FILE_NAME=snapshot.tar.gz
 SNAPSHOT_DIR=/data
 
 LATEST_SNAPSHOT_FILE_NAME=$(curl https://$NETWORK_ID-$NODE_CONFIG-snapshots.base.org/latest) && \
-s5cmd --log error cp s3://base-snapshots-$NETWORK_ID-archive/$LATEST_SNAPSHOT_FILE_NAME /data && \
+s5cmd --log error cp s3://base-snapshots-$NETWORK_ID-archive/$LATEST_SNAPSHOT_FILE_NAME $SNAPSHOT_DIR/$SNAPSHOT_FILE_NAME && \
 echo "Downloading Snapshot script finished" && \
 sleep 60 &&\
 echo "Starting snapshot decompression ..." && \
@@ -19,13 +19,13 @@ tar -zxvf  $SNAPSHOT_DIR/$SNAPSHOT_FILE_NAME -C /data 2>&1 | tee unzip.log && ec
 
 echo "Decompresed snapshot, cleaning up..."
 
-mv /data/snapshots/$NETWORK_ID/download/* /data && \
+mv /data/snapshots/$NETWORK_ID/download/* $SNAPSHOT_DIR && \
 rm -rf /data/snapshots && \
 rm -rf $SNAPSHOT_DIR/$SNAPSHOT_FILE_NAME
 
 echo "Snapshot is ready, starting the service.."
 
-chown -R bcuser:bcuser /data
+chown -R bcuser:bcuser $SNAPSHOT_DIR
 
 sudo systemctl daemon-reload
 sudo systemctl enable --now base
