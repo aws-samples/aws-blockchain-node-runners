b0966227f42233547535de3166f42e27635cba3f
Ahmed Ghonima <25593897+AhGhanima@users.noreply.github.com>
2024-07-01 10:51:14 +0200

    PR comments for tezos node

--- lib/tezos/lib/assets/user-data/node.sh
+++ lib/tezos/lib/assets/user-data/node.sh
@@ -140,7 +140,7 @@ fi
 
 
 if [[ "$INSTANCE_TYPE" == "HA" ]]; then
-  su tezos -c "aws s3 sync s3://$S3_SYNC_BUCKET/node ~/.tezos-node/node"
+  su tezos -c "s5cmd sync s3://$S3_SYNC_BUCKET/node ~/.tezos-node/node"
 fi
 
 if [[ "$INSTANCE_TYPE" == "SNAPSHOT" ]]; then
