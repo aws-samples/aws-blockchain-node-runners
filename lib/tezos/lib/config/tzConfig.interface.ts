37696648c59368427002644a48296b5372f82c7f
Christoph Niemann <christoph@niemannsland.de>
2024-07-01 12:51:14 +0200

    Make the download URIs for the binary configurable

--- lib/tezos/lib/config/tzConfig.interface.ts
+++ lib/tezos/lib/config/tzConfig.interface.ts
@@ -3,6 +3,8 @@ import * as configTypes from "../../../constructs/config.interface";
 export type TzNetwork = "mainnet" | "sandbox" | "ghostnet";
 export type TzNodeHistoryMode = "full" | "rolling" | "archive";
 
+export type TzSnapshotRegion = "eu" | "us" | "asia";
+
 export type TzNodeRole = "sync-node" | "rpc-node" | "single-node";
 
 export interface TzDataVolumeConfig extends configTypes.DataVolumeConfig {
@@ -15,9 +17,11 @@ export interface TzBaseConfig extends configTypes.BaseConfig {
 export interface TzBaseNodeConfig extends configTypes.BaseNodeConfig {
     tzNetwork: TzNetwork;
     historyMode: TzNodeHistoryMode;
+    snapshotRegion: TzSnapshotRegion;
     snapshotsUrl: string;
     dataVolume: TzDataVolumeConfig;
     downloadSnapshot: string;
+    octezDownloadUri: string;
 }
 
 export interface TzHAConfig {
--- lib/tezos/lib/config/tzConfig.ts
+++ lib/tezos/lib/config/tzConfig.ts
@@ -1,6 +1,7 @@
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as configTypes from "./tzConfig.interface";
 import * as constants from "../../../constructs/constants";
+import { arch } from "os";
 
 const parseDataVolumeType = (dataVolumeType: string) => {
     switch (dataVolumeType) {
@@ -22,12 +23,50 @@ export const baseConfig: configTypes.TzBaseConfig = {
     region: process.env.AWS_REGION || "us-east-1"
 };
 
+const getArch = (cpuType: string | undefined) => {
+    switch (cpuType?.toLowerCase()) {
+        case "x86_64":
+            return ec2.AmazonLinuxCpuType.X86_64;
+        case "arm64":
+            return ec2.AmazonLinuxCpuType.ARM_64;
+        default:
+            return ec2.AmazonLinuxCpuType.X86_64;
+    }
+}
+
+const getDownloadUri = (arch: ec2.AmazonLinuxCpuType) => {
+    // defaults to v0.20
+    switch (arch) {
+        case ec2.AmazonLinuxCpuType.X86_64:
+            return process.env.TZ_X86_OCTEZ_URI || "https://gitlab.com/tezos/tezos/-/package_files/133747462/download";
+        case ec2.AmazonLinuxCpuType.ARM_64:
+            return process.env.TZ_ARM64_OCTEZ_URI || "https://gitlab.com/tezos/tezos/-/package_files/133748628/download";
+        default:
+            return process.env.TZ_X86_OCTEZ_URI || "https://gitlab.com/tezos/tezos/-/package_files/133747462/download";
+    }
+}
+
+const getSnapshotUri = (network: configTypes.TzNetwork, historyMode: configTypes.TzNodeHistoryMode, snapshotRegion: configTypes.TzSnapshotRegion, snapshotsUrl: string) => {
+    if (snapshotsUrl !== constants.NoneValue) {
+        return snapshotsUrl;
+    }
+
+    return `https://snapshots.${snapshotRegion}.tzinit.org/${network}/${historyMode}`
+}
+
 export const baseNodeConfig: configTypes.TzBaseNodeConfig = {
     instanceType: new ec2.InstanceType(process.env.TZ_INSTANCE_TYPE ? process.env.TZ_INSTANCE_TYPE : "c7g.xlarge"),
-    instanceCpuType: process.env.TZ_CPU_TYPE?.toLowerCase() == "x86_64" ? ec2.AmazonLinuxCpuType.X86_64 : ec2.AmazonLinuxCpuType.ARM_64 ,
-    tzNetwork: <configTypes.TzNetwork>process.env.TZ_CLUSTER || "mainnet",
+    instanceCpuType: getArch(process.env.TZ_CPU_TYPE),
+    tzNetwork: <configTypes.TzNetwork>process.env.TZ_NETWORK || "mainnet",
     historyMode: <configTypes.TzNodeHistoryMode>process.env.TZ_HISTORY_MODE || "full",
-    snapshotsUrl: process.env.TZ_SNAPSHOT_URI || constants.NoneValue,
+    snapshotRegion: <configTypes.TzSnapshotRegion>process.env.TZ_SNAPSHOT_REGION || "us",
+    octezDownloadUri: getDownloadUri(getArch(process.env.TZ_CPU_TYPE)),
+    snapshotsUrl: getSnapshotUri(
+        <configTypes.TzNetwork>process.env.TZ_NETWORK || "mainnet",
+        <configTypes.TzNodeHistoryMode>process.env.TZ_HISTORY_MODE || "full",
+        <configTypes.TzSnapshotRegion>process.env.TZ_SNAPSHOT_REGION || "us",
+        process.env.TZ_SNAPSHOTS_URL || constants.NoneValue
+    ),
     dataVolume: {
         sizeGiB: process.env.TZ_DATA_VOL_SIZE ? parseInt(process.env.TZ_DATA_VOL_SIZE) : 2000,
         type: parseDataVolumeType(process.env.TZ_DATA_VOL_TYPE?.toLowerCase() ? process.env.TZ_DATA_VOL_TYPE?.toLowerCase() : "gp3"),
