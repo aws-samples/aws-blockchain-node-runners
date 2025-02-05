4429cfb8fa1c9b8f9edda000d0e7a248ba4c4364
Nikolay Vlasov <vlason@amazon.com>
2025-02-05 17:04:51 +1100

    Solana. Increased p2p port range and modified samples to support new version of Agave client

--- lib/solana/lib/assets/node/node-extended-rpc-template.sh
+++ lib/solana/lib/assets/node/node-extended-rpc-template.sh
@@ -24,7 +24,7 @@ __ENTRY_POINTS__ \
 --full-rpc-api \
 --rpc-port 8899 \
 --gossip-port 8800 \
---dynamic-port-range 8800-8814 \
+--dynamic-port-range 8800-8816 \
 --private-rpc \
 --rpc-bind-address $EC2_INTERNAL_IP \
 --wal-recovery-mode skip_any_corrupted_record \
