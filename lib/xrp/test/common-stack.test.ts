4940106f248b6202ad8cfb94abd170f27df6c65d
Pedro Aceves <acevespa@amazon.com>
2025-02-06 09:17:03 -0700

    add tests

---
+++ lib/xrp/test/common-stack.test.ts
@@ -0,0 +1,76 @@
+import { Template } from "aws-cdk-lib/assertions";
+import * as cdk from "aws-cdk-lib";
+import * as dotenv from "dotenv";
+import * as config from "../lib/config/XRPConfig";
+import { XRPCommonStack } from "../lib/common-stack";
+
+dotenv.config({ path: './test/.env-test' });
+
+describe("SolanaCommonStack", () => {
+    test("synthesizes the way we expect", () => {
+        const app = new cdk.App();
+
+        // Create the SolanaCommonStack.
+        const xrpCommonStack = new XRPCommonStack(app, "xrp-common", {
+            env: { account: config.baseConfig.accountId, region: config.baseConfig.region },
+            stackName: `xrp-nodes-common`,
+        });
+
+        // Prepare the stack for assertions.
+        const template = Template.fromStack(xrpCommonStack);
+
+        // Has EC2 instance role.
+        template.hasResourceProperties("AWS::IAM::Role", {
+            AssumeRolePolicyDocument: {
+                Statement: [
+                    {
+                        Action: "sts:AssumeRole",
+                        Effect: "Allow",
+                        Principal: {
+                            Service: "ec2.amazonaws.com"
+                        }
+                    }
+                ]
+            },
+            ManagedPolicyArns: [
+                {
+                    "Fn::Join": [
+                        "",
+                        [
+                            "arn:",
+                            {
+                                "Ref": "AWS::Partition"
+                            },
+                            ":iam::aws:policy/SecretsManagerReadWrite"
+                        ]
+                    ]
+                },
+                {
+                    "Fn::Join": [
+                        "",
+                        [
+                            "arn:",
+                            {
+                                Ref: "AWS::Partition"
+                            },
+                            ":iam::aws:policy/AmazonSSMManagedInstanceCore"
+                        ]
+                    ]
+                },
+                {
+                    "Fn::Join": [
+                        "",
+                        [
+                            "arn:",
+                            {
+                                "Ref": "AWS::Partition"
+                            },
+                            ":iam::aws:policy/CloudWatchAgentServerPolicy"
+                        ]
+                    ]
+                }
+            ]
+        })
+
+    });
+});
---
+++ lib/xrp/test/single-node-stack.test.ts
@@ -0,0 +1,118 @@
+import { Match, Template } from "aws-cdk-lib/assertions";
+import * as cdk from "aws-cdk-lib";
+import * as dotenv from "dotenv";
+import * as config from "../lib/config/XRPConfig";
+import { XRPCommonStack } from "../lib/common-stack";
+import { XRPSingleNodeStack } from "../lib/single-node-stack";
+
+dotenv.config({ path: './test/.env-test' });
+
+describe("SolanaSingleNodeStack", () => {
+    test("synthesizes the way we expect", () => {
+        const app = new cdk.App();
+        const xrpCommonStack = new XRPCommonStack(app, "xrp-common", {
+            env: { account: config.baseConfig.accountId, region: config.baseConfig.region },
+            stackName: `xrp-nodes-common`,
+        });
+
+        // Create the XRPSingleNodeStack.
+        const xrpSingleNodeStack = new XRPSingleNodeStack(app, "XRP-sync-node", {
+            env: { account: config.baseConfig.accountId, region: config.baseConfig.region },
+            stackName: `XRP-single-node`,
+            instanceType: config.baseNodeConfig.instanceType,
+            instanceCpuType: config.baseNodeConfig.instanceCpuType,
+            dataVolume: config.baseNodeConfig.dataVolume,
+            hubNetworkID: config.baseNodeConfig.hubNetworkID,
+            instanceRole: xrpCommonStack.instanceRole,
+        });
+
+        // Prepare the stack for assertions.
+        const template = Template.fromStack(xrpSingleNodeStack);
+
+        // Has EC2 instance security group.
+        template.hasResourceProperties("AWS::EC2::SecurityGroup", {
+            GroupDescription: Match.anyValue(),
+            VpcId: Match.anyValue(),
+            SecurityGroupEgress: [
+                {
+                    "CidrIp": "0.0.0.0/0",
+                    "Description": "Allow all outbound traffic by default",
+                    "IpProtocol": "-1"
+                }
+            ],
+            SecurityGroupIngress: [
+                {
+                    "CidrIp": "0.0.0.0/0",
+                    "Description": "P2P protocols",
+                    "FromPort": 51235,
+                    "IpProtocol": "tcp",
+                    "ToPort": 51235
+                },
+                {
+                    "CidrIp": "0.0.0.0/0",
+                    "Description": "P2P protocols",
+                    "FromPort": 2459,
+                    "IpProtocol": "tcp",
+                    "ToPort": 2459
+                },
+                {
+                    "CidrIp": "1.2.3.4/5",
+                    "Description": "RPC port HTTP (user access needs to be restricted. Allowed access only from internal IPs)",
+                    "FromPort": 6005,
+                    "IpProtocol": "tcp",
+                    "ToPort": 6005
+                }
+            ]
+        })
+
+        // Has EC2 instance with node configuration
+        template.hasResourceProperties("AWS::EC2::Instance", {
+            AvailabilityZone: Match.anyValue(),
+            UserData: Match.anyValue(),
+            BlockDeviceMappings: [
+                {
+                    DeviceName: "/dev/xvda",
+                    Ebs: {
+                        DeleteOnTermination: true,
+                        Encrypted: true,
+                        Iops: 3000,
+                        VolumeSize: 46,
+                        VolumeType: "gp3"
+                    }
+                }
+            ],
+            IamInstanceProfile: Match.anyValue(),
+            ImageId: Match.anyValue(),
+            InstanceType: "r7a.12xlarge",
+            Monitoring: true,
+            PropagateTagsToVolumeOnCreation: true,
+            SecurityGroupIds: Match.anyValue(),
+            SubnetId: Match.anyValue(),
+        })
+
+        // Has EBS data volume.
+        template.hasResourceProperties("AWS::EC2::Volume", {
+            AvailabilityZone: Match.anyValue(),
+            Encrypted: true,
+            Iops: 12000,
+            MultiAttachEnabled: false,
+            Size: 2000,
+            Throughput: 700,
+            VolumeType: "gp3"
+        })
+
+        // Has EBS data volume attachment.
+        template.hasResourceProperties("AWS::EC2::VolumeAttachment", {
+            Device: "/dev/sdf",
+            InstanceId: Match.anyValue(),
+            VolumeId: Match.anyValue(),
+        })
+
+        // Has CloudWatch dashboard.
+        template.hasResourceProperties("AWS::CloudWatch::Dashboard", {
+            DashboardBody: Match.anyValue(),
+            DashboardName: {"Fn::Join": ["", ["XRP-single-node-",{ "Ref": Match.anyValue() }]]}
+        })
+
+    });
+});
