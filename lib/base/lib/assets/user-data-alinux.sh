0295682d2b92903f6a7dc41e31bb9d738212c17c
Nikolay Vlasov <vlason@amazon.com>
2025-04-08 15:21:39 +1000

    Base. Upgrade and refactored to address common operational problems

--- lib/base/lib/assets/user-data-alinux.sh
+++ lib/base/lib/assets/user-data-alinux.sh
@@ -8,13 +8,36 @@ RESOURCE_ID=${_NODE_CF_LOGICAL_ID_}
 ASSETS_S3_PATH=${_ASSETS_S3_PATH_}
 DATA_VOLUME_TYPE=${_DATA_VOLUME_TYPE_}
 DATA_VOLUME_SIZE=${_DATA_VOLUME_SIZE_}
+
+# Set by Base-specic CDK components and stacks
+AWS_REGION=${_AWS_REGION_}
+STACK_NAME=${_STACK_NAME_}
+RESTORE_FROM_SNAPSHOT=${_RESTORE_FROM_SNAPSHOT_}
+NETWORK_ID=${_NETWORK_ID_}
+NODE_CONFIG=${_NODE_CONFIG_}
+L1_EXECUTION_ENDPOINT=${_L1_EXECUTION_ENDPOINT_}
+L1_CONSENSUS_ENDPOINT=${_L1_CONSENSUS_ENDPOINT_}
+SNAPSHOT_URL=${_SNAPSHOT_URL_}
+
 {
   echo "LIFECYCLE_HOOK_NAME=$LIFECYCLE_HOOK_NAME"
   echo "AUTOSCALING_GROUP_NAME=$AUTOSCALING_GROUP_NAME"
   echo "ASSETS_S3_PATH=$ASSETS_S3_PATH"
   echo "DATA_VOLUME_TYPE=$DATA_VOLUME_TYPE"
   echo "DATA_VOLUME_SIZE=$DATA_VOLUME_SIZE"
-} >> /etc/environment
+
+  echo "AWS_REGION=$AWS_REGION"
+  echo "NETWORK_ID=$NETWORK_ID"
+  echo "NODE_CONFIG=$NODE_CONFIG"
+  echo "L1_EXECUTION_ENDPOINT=$L1_EXECUTION_ENDPOINT"
+  echo "L1_CONSENSUS_ENDPOINT=$L1_CONSENSUS_ENDPOINT"
+  echo "SNAPSHOT_URL=$SNAPSHOT_URL"
+} >> /etc/cdk_environment
+
+source /etc/cdk_environment
+
+# Export environment variables so calls to `envsubst` inherit the evironment variables.
+while read -r line; do export "$line"; done < /etc/cdk_environment
 
 arch=$(uname -m)
 
@@ -22,20 +45,21 @@ echo "Architecture detected: $arch"
 
 if [ "$arch" == "x86_64" ]; then
   SSM_AGENT_BINARY_URI=https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-  AWS_CLI_BINARY_URI=https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
   S5CMD_URI=https://github.com/peak/s5cmd/releases/download/v2.1.0/s5cmd_2.1.0_Linux-64bit.tar.gz
   YQ_URI=https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
 else
   SSM_AGENT_BINARY_URI=https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_arm64/amazon-ssm-agent.rpm
-  AWS_CLI_BINARY_URI=https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip
   S5CMD_URI=https://github.com/peak/s5cmd/releases/download/v2.1.0/s5cmd_2.1.0_Linux-arm64.tar.gz
   YQ_URI=https://github.com/mikefarah/yq/releases/latest/download/yq_linux_arm64
 fi
 
 echo "Updating and installing required system packages"
-yum update -y
-yum -y install amazon-cloudwatch-agent collectd jq yq gcc ncurses-devel aws-cfn-bootstrap zstd
+dnf update -y
+dnf -y install amazon-cloudwatch-agent collectd jq gcc ncurses-devel telnet aws-cfn-bootstrap cronie zstd git
 wget $YQ_URI -O /usr/bin/yq && chmod +x /usr/bin/yq
+
+sudo systemctl enable crond.service
+sudo systemctl start crond.service
 
 echo " Installing aria2 a p2p downloader"
 cd /tmp
@@ -66,76 +90,18 @@ echo "Downloading assets zip file"
 aws s3 cp $ASSETS_S3_PATH ./assets.zip
 unzip -q assets.zip
 
-echo 'Uninstalling AWS CLI v1'
-yum remove awscli
-
-echo 'Installing AWS CLI v2'
-curl $AWS_CLI_BINARY_URI -o "awscliv2.zip"
-unzip -q awscliv2.zip
-./aws/install
-rm /usr/bin/aws
-ln /usr/local/bin/aws /usr/bin/aws
-
-echo 'Installing SSM Agent'
+echo 'Upgrading SSM Agent'
 yum install -y $SSM_AGENT_BINARY_URI
 
 # Base specific setup starts here
 
-# Set by Base-specic CDK components and stacks
-REGION=${_REGION_}
-STACK_NAME=${_STACK_NAME_}
-RESTORE_FROM_SNAPSHOT=${_RESTORE_FROM_SNAPSHOT_}
-NETWORK_ID=${_NETWORK_ID_}
-NODE_CONFIG=${_NODE_CONFIG_}
-L1_EXECUTION_ENDPOINT=${_L1_EXECUTION_ENDPOINT_}
-L1_CONSENSUS_ENDPOINT=${_L1_CONSENSUS_ENDPOINT_}
-SNAPSHOT_URL=${_SNAPSHOT_URL_}
-
-{
-  echo "REGION=$REGION"
-  echo "NETWORK_ID=$NETWORK_ID"
-  echo "NODE_CONFIG=$NODE_CONFIG"
-  echo "L1_EXECUTION_ENDPOINT=$L1_EXECUTION_ENDPOINT"
-  echo "L1_CONSENSUS_ENDPOINT=$L1_CONSENSUS_ENDPOINT"
-  echo "SNAPSHOT_URL=$SNAPSHOT_URL"
-} >> /etc/environment
-
-GIT_URL=https://github.com/base-org/node.git
-SYNC_CHECKER_FILE_NAME=syncchecker-base.sh
-
-yum -y install docker python3-pip cronie cronie-anacron gcc python3-devel git
-yum -y remove python-requests
-pip3 install docker-compose
-pip3 install hapless
-pip3 uninstall -y urllib3
-pip3 install 'urllib3<2.0'
-
-echo "Assigning Swap Space"
-# Check if a swap file already exists
-if [ -f /swapfile ]; then
-  # Remove the existing swap file
-  swapoff /swapfile
-  rm -rf /swapfile
-fi
-
-# Create a new swap file
-# Set swap size to fixed 5 GB
-swap_size_mb=5120
-unit=M
-fallocate -l $swap_size_mb$unit /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-
-# Enable the swap space to persist after reboot.
-echo "/swapfile none swap sw 0 0" | sudo tee -a /etc/fstab
-
-sysctl vm.swappiness=6
-sysctl vm.vfs_cache_pressure=10
-echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf
-echo "vm.vfs_cache_pressure=10"  | sudo tee -a /etc/sysctl.conf
-
-free -h
+echo "Installing Docker"
+dnf remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sed -i 's/$releasever/9/g' /etc/yum.repos.d/docker-ce.repo
+dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl enable --now docker
 
 mkdir -p /data
 
@@ -152,6 +118,7 @@ systemctl enable docker
 
 echo "Clonning node repo"
 cd /home/bcuser
+GIT_URL=https://github.com/base-org/node.git
 git clone $GIT_URL
 cd ./node
 
@@ -208,21 +175,18 @@ sed -i "s#GETH_HOST_DATA_DIR=./geth-data#GETH_HOST_DATA_DIR=/data/geth#g" /home/
 
 chown -R bcuser:bcuser /home/bcuser/node
 
-echo "Configuring syncchecker script"
-cp /opt/sync-checker/$SYNC_CHECKER_FILE_NAME /opt/syncchecker.sh
-chmod 766 /opt/syncchecker.sh
-
-echo "*/5 * * * * /opt/syncchecker.sh" | crontab
-crontab -l
+echo "Configuring and starting sync-checker"
+SYNC_CHECKER_FILE_NAME="syncchecker-base.sh"
+/opt/sync-checker/setup.sh "/opt/sync-checker/$SYNC_CHECKER_FILE_NAME"
 
 echo "Configuring node as a service"
 mkdir /home/bcuser/bin
-mv /opt/base/node-start.sh /home/bcuser/bin/node-start.sh
-mv /opt/base/node-stop.sh /home/bcuser/bin/node-stop.sh
+mv /opt/node/node-start.sh /home/bcuser/bin/node-start.sh
+mv /opt/node/node-stop.sh /home/bcuser/bin/node-stop.sh
 chmod 766 /home/bcuser/bin/*
 chown -R bcuser:bcuser /home/bcuser
 
-sudo bash -c 'cat > /etc/systemd/system/base.service <<EOF
+sudo bash -c 'cat > /etc/systemd/system/node.service <<EOF
 [Unit]
 Description=Base Node
 After=network-online.target
@@ -238,46 +202,32 @@ WantedBy=multi-user.target
 EOF'
 
 if [[ "$LIFECYCLE_HOOK_NAME" == "none" ]]; then
-  echo "We run single node setup. Signaling completion to CloudFormation to continue with volume mount"
-  /opt/aws/bin/cfn-signal --stack $STACK_NAME --resource $RESOURCE_ID --region $REGION
+    #If cfn-signal is not available, install it
+  if ! command -v cfn-signal &> /dev/null
+  then
+    echo "cfn-signal could not be found, installing"
+    /opt/instance/cfn-hup/setup.sh "$STACK_NAME" "$AWS_REGION"
+  else
+    echo "cfn-signal is available, skipping installation"
+  fi
+  cfn-signal --stack "$STACK_NAME" --resource "$RESOURCE_ID" --region "$AWS_REGION"
 fi
 
 echo "Preparing data volume"
 
-echo "Wait for one minute for the volume to become available"
-sleep 60s
-
 if [[ "$DATA_VOLUME_TYPE" == "instance-store" ]]; then
   echo "Data volume type is instance store"
 
-  cd /opt
-  chmod +x /opt/setup-instance-store-volumes.sh
-
-  (crontab -l; echo "@reboot /opt/setup-instance-store-volumes.sh >/tmp/setup-instance-store-volumes.log 2>&1") | crontab -
+  (crontab -l; echo "@reboot /opt/instance/storage/setup.sh /data ext4 > /tmp/setup-store-volume-data.log 2>&1") | crontab -
   crontab -l
 
-  DATA_VOLUME_ID=/dev/$(lsblk -lnb | awk 'max < $4 {max = $4; vol = $1} END {print vol}')
-
+  /opt/instance/storage/setup.sh /data ext4
 else
   echo "Data volume type is EBS"
-
-  DATA_VOLUME_ID=/dev/$(lsblk -lnb | awk -v VOLUME_SIZE_BYTES="$DATA_VOLUME_SIZE" '{if ($4== VOLUME_SIZE_BYTES) {print $1}}')
+  echo "Waiting for EBS volume to become available"
+  sleep 60
+  /opt/instance/storage/setup.sh /data ext4
 fi
-
-mkfs -t ext4 $DATA_VOLUME_ID
-echo "waiting for volume to get UUID"
-  OUTPUT=0;
-  while [ "$OUTPUT" = 0 ]; do
-    DATA_VOLUME_UUID=$(lsblk -fn -o UUID $DATA_VOLUME_ID)
-    OUTPUT=$(echo $DATA_VOLUME_UUID | grep -c - $2)
-    echo $OUTPUT
-  done
-DATA_VOLUME_FSTAB_CONF="UUID=$DATA_VOLUME_UUID /data ext4 defaults 0 2"
-echo "DATA_VOLUME_ID="$DATA_VOLUME_ID
-echo "DATA_VOLUME_UUID="$DATA_VOLUME_UUID
-echo "DATA_VOLUME_FSTAB_CONF="$DATA_VOLUME_FSTAB_CONF
-echo $DATA_VOLUME_FSTAB_CONF | tee -a /etc/fstab
-mount -a
 
 lsblk -d
 
@@ -295,18 +245,18 @@ systemctl restart amazon-cloudwatch-agent
 if [ "$RESTORE_FROM_SNAPSHOT" == "false" ]; then
   echo "Skipping restoration from snapshot. Starting node"
   systemctl daemon-reload
-  systemctl enable --now base
+  systemctl enable --now node
 else
   echo "Restoring full node from snapshot over http"
-  chmod +x /opt/restore-from-snapshot-http.sh
-  echo "/opt/restore-from-snapshot-http.sh" | at now + 1 min
+  chmod +x /opt/instance/storage/restore-from-snapshot-http.sh
+  echo "/opt/instance/storage/restore-from-snapshot-http.sh" | at now + 1 min
 fi
 
 if [[ "$LIFECYCLE_HOOK_NAME" != "none" ]]; then
   echo "Signaling ASG lifecycle hook to complete"
   TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
   INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/instance-id)
-  aws autoscaling complete-lifecycle-action --lifecycle-action-result CONTINUE --instance-id $INSTANCE_ID --lifecycle-hook-name "$LIFECYCLE_HOOK_NAME" --auto-scaling-group-name "$AUTOSCALING_GROUP_NAME"  --region $REGION
+  aws autoscaling complete-lifecycle-action --lifecycle-action-result CONTINUE --instance-id $INSTANCE_ID --lifecycle-hook-name "$LIFECYCLE_HOOK_NAME" --auto-scaling-group-name "$AUTOSCALING_GROUP_NAME"  --region $AWS_REGION
 fi
 
 echo "All Done!!"
--- lib/base/lib/ha-nodes-stack.ts
+++ lib/base/lib/ha-nodes-stack.ts
@@ -31,7 +31,7 @@ export class BaseHANodesStack extends cdk.Stack {
     constructor(scope: cdkConstructs.Construct, id: string, props: BaseHANodesStackProps) {
         super(scope, id, props);
 
-        const REGION = cdk.Stack.of(this).region;
+        const AWS_REGION = cdk.Stack.of(this).region;
         const STACK_NAME = cdk.Stack.of(this).stackName;
         const lifecycleHookName = STACK_NAME;
         const autoScalingGroupName = STACK_NAME;
@@ -69,11 +69,11 @@ export class BaseHANodesStack extends cdk.Stack {
         asset.bucket.grantRead(instanceRole);
 
         // parsing user data script and injecting necessary variables
-        const nodeScript = fs.readFileSync(path.join(__dirname, "assets", "user-data", "node.sh")).toString();
+        const nodeScript = fs.readFileSync(path.join(__dirname, "assets", "user-data-alinux.sh")).toString();
         const dataVolumeSizeBytes = dataVolume.sizeGiB * constants.GibibytesToBytesConversionCoefficient;
 
         const modifiedInitNodeScript = cdk.Fn.sub(nodeScript, {
-            _REGION_: REGION,
+            _AWS_REGION_: AWS_REGION,
             _ASSETS_S3_PATH_: `s3://${asset.s3BucketName}/${asset.s3ObjectKey}`,
             _STACK_NAME_: STACK_NAME,
             _NODE_CF_LOGICAL_ID_: constants.NoneValue,
@@ -94,9 +94,9 @@ export class BaseHANodesStack extends cdk.Stack {
             instanceType,
             dataVolumes: [dataVolume],
             machineImage: new ec2.AmazonLinuxImage({
-                generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
-                kernel:ec2.AmazonLinuxKernel.KERNEL5_X,
-                cpuType: instanceCpuType
+                generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023,
+                kernel:ec2.AmazonLinuxKernel.KERNEL6_1,
+                cpuType: instanceCpuType,
             }),
             role: instanceRole,
             vpc,
